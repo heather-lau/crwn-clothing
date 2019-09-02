321b15f6a9a7490cd019bdff438771871fd4c443
Heather <i.heatherlau@gmail.com>
2019-09-02 12:05:06 +0800

    Update firebase utils

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,12 +13,14 @@ const config = {
 }
 
 firebase.initializeApp(config)
+export const auth = firebase.auth()
+export const firestore = firebase.firestore()
 
+// Create user
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
 
   const userRef = firestore.doc(`users/${userAuth.uid}`)
-
   const snapShot = await userRef.get()
 
   if (!snapShot.exists) {
@@ -40,9 +42,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   return userRef
 }
 
-export const auth = firebase.auth()
-export const firestore = firebase.firestore()
-
+// Sign in with Google method
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' })
 export const signInWithGoogle = () => auth.signInWithPopup(provider)
